a7dc8828bbe954acaa0e82b406cc21357af9ee3e
rajeevthottunkal <rajeevthottunkal@gmail.com>
2024-11-04 17:26:25 +0530

    second commit

---
+++ src/App.jsx
@@ -0,0 +1,65 @@
+import SideBar from "./components/SideBar";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
+import { useEffect, useState } from "react";
+
+function App() {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+
+  function handleToggleModal() {
+    setShowModal(!showModal);
+  }
+
+  useEffect(() => {
+    async function fetchAPIData() {
+      const NASA_KEY = import.meta.env.VITE_NASA_API_KEY;
+      const url =
+        "https://api.nasa.gov/planetary/apod" +
+        "?api_key=9lw9Hp25c0B7NQeKM8myWWDraknAmpIcQBNO7qHn";
+
+      const today = new Date().toDateString();
+      const localkey = `NASA-${today}`;
+
+      if (localStorage.getItem(localkey)) {
+        const apiData = JSON.parse(localStorage.getItem(localkey));
+        setData(apiData);
+        console.log("Fetched from cache today")
+        return;
+      }
+      localStorage.clear();
+
+      try {
+        const res = await fetch(url);
+        const apiData = await res.json();
+        localStorage.setItem(localkey, JSON.stringify(apiData));
+        setData(apiData);
+        console.log("DATA\n", apiData);
+        console.log("Fetched from API today")
+
+      } catch (err) {
+        console.log(err.message);
+      }
+    }
+    fetchAPIData();
+  }, []);
+
+  return (
+    <>
+      {data ? (
+        <Main data={data} />
+      ) : (
+        <div className="loadingState">
+          <i className="fa-solid fa-gear"></i>
+        </div>
+      )}{" "}
+      {showModal && (
+        <SideBar data={data} handleToggleModal={handleToggleModal} />
+      )}
+      {data && <Footer data={data} handleToggleModal={handleToggleModal} />}{" "}
+    </>
+  );
+}
+
+export default App;
